f8713225374e2f444c30de5f142b80d4d45019f1
MostakemHossain <mostakememon123@gmail.com>
2024-10-04 22:05:47 +0600

    env issue

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,17 +1,21 @@
-// firebaseConfig.ts
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCrjgydiEWbpKbjL_ccDmh47woViO2tCiM",
+  authDomain: "sports-edge-e41ea.firebaseapp.com",
+  projectId: "sports-edge-e41ea",
+  storageBucket: "sports-edge-e41ea.appspot.com",
+  messagingSenderId: "295722556017",
+  appId: "1:295722556017:web:61a6ff82c3b7451a51d9d0",
 };
 
 // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
